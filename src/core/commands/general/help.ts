68ed051eba3d43a79297e038250d9e35c6c7af2e
mrnothingslee-sudo <67931916+mrnothingslee-sudo@users.noreply.github.com>
2022-03-01 12:55:44 -0500

    Update help.ts

--- src/core/commands/general/help.ts
+++ src/core/commands/general/help.ts
@@ -1,10 +1,21 @@
 import { Command } from "../../Command";
+import { Message } from "discord.js";
+const Discord = require("discord.js");
 
 export default class Help extends Command {
     constructor (client) {
         super(client, {
             name: "help",
-            description: "gfsg"
-        })
-    }
+            description: "Displays a list of all current commands, sorted by category."
+        });
+    };
+
+    async run(message: Message) {
+const helphelpcpmmands = this.client.commands.map(cmd => cmd.name).join("`, `")
+
+const embed = new Discord.MessageEmbed()
+.setTitle(`Help Menu`)
+.setDescription(`\`${helphelpcpmmands}\``)
+message.channel.send({ embeds: [embed]})
+    };
 };
